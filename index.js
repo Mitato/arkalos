a7d7a81f73f6b1e31d95b66eebbf1dcc244233d3
diogenisAl <diogenis_Al@yahoo.com>
2017-01-18 16:15:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// AM: P2018000, Name/Surname: ... 
-// The code is written in node.js
-
-<Ο κώδικας του χρήστη Π2018000>
-
-
-// AM: P2018001, Name/Surname: ... 
-// The code is written in ruby
-
-<Ο κώδικας του χρήστη Π2018001>
-
-
-// ...
+// testing twitter project
